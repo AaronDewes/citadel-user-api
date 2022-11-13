bdb646b97f84cdeb849e5ccc707ce29082739340
Aaron Dewes <aaron.dewes@protonmail.com>
2022-11-13 12:30:09 +0100

    Add migrated users counter

---
+++ supabase/functions/add-umbrel-user/index.ts
@@ -0,0 +1,26 @@
+import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@^2.0.0'
+
+const supabase = createClient(
+  Deno.env.get('SUPABASE_URL') ?? '',
+  Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
+)
+
+serve(async (req) => {
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+
+  const { error } = await supabase.from("umbrel_users").insert({});
+
+  if (error) {
+  	console.error(error);
+  }
+
+  return new Response(
+    JSON.stringify({
+    	success: true,
+    }),
+    { headers: { ...corsHeaders, "Content-Type": "application/json" } },
+  )
+});
---
+++ supabase/functions/get-umbrel-users/index.ts
@@ -0,0 +1,37 @@
+import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@^2.0'
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey',
+};
+
+const supabase = createClient(
+  Deno.env.get('SUPABASE_URL') ?? '',
+  Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
+)
+
+serve(async (req) => {
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+
+  const { data, error } = await supabase.from("umbrel_users").select();
+
+  if (error) {
+  	console.error(error);
+  }
+
+  return new Response(
+    JSON.stringify({
+    	userCount: data?.length || 0,
+    }),
+    { headers: { ...corsHeaders, "Content-Type": "application/json" } },
+  )
+});
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
